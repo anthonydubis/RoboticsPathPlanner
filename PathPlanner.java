fec2e09918bea172247467412f3b9973bf5f5bc7
lillyfwang <lillyfwang@yahoo.com>
2015-11-15 01:31:48 -0500

    Added reflection and convex hull

--- PathPlanner.java
+++ PathPlanner.java
@@ -100,6 +100,9 @@ class PointsPanel extends JPanel
 	Point goal;
 	Obstacle world;
 	ArrayList<Obstacle> obstacles = new ArrayList<Obstacle>();
+	ArrayList<Obstacle> grownObstacles = new ArrayList<Obstacle>();
+	final int SCALE_FACTOR = 50;
+	final int ROBOT_SIZE = (int)(0.35 * SCALE_FACTOR);
 
     public PointsPanel(Point st, Point gl, Obstacle wd, ArrayList<Obstacle> obs) {
 		setBackground(getBackground());
@@ -115,9 +118,11 @@ class PointsPanel extends JPanel
         g.setColor(getBackground()); //colors the window
         g.fillRect(0, 0, getWidth(), getHeight());
         g.setColor(getForeground()); //set color and fonts
-        drawBorders(g, world.getVerticies());
-        drawObstacles(g);
+        drawBorders(g, world.getVerticies(), Color.blue);
+        drawObstacles(g, obstacles, Color.blue);
         drawStartGoal(g);
+        growObstacles(g);
+        drawObstacles(g, grownObstacles, Color.yellow);
         revalidate();
     }
 
@@ -133,13 +138,13 @@ class PointsPanel extends JPanel
     	g.fillRect(goal_y, goal_x, 5, 5);
     }
 
-    public void drawObstacles(Graphics g) {
-    	for (int a = 0; a < obstacles.size(); a++) {
-    		drawBorders(g, obstacles.get(a).getVerticies());
+    public void drawObstacles(Graphics g, ArrayList<Obstacle> obs, Color color) {
+    	for (int a = 0; a < obs.size(); a++) {
+    		drawBorders(g, obs.get(a).getVerticies(), color);
     	}
     }
 
-    public void drawBorders(Graphics g, ArrayList<Point> vertices) {
+    public void drawBorders(Graphics g, ArrayList<Point> vertices, Color color) {
     	for (int i = 0; i < vertices.size()-1; i++) {
         	Point a = vertices.get(i);
         	int x1 = dataToMapCoord(a.getX());
@@ -158,8 +163,111 @@ class PointsPanel extends JPanel
     		}
 
 			// Switching x and y so it can fit the screen better
-            g.setColor(Color.blue);
+            g.setColor(color);
             g.drawLine(y1, x1, y2, x2);
+        }
+    }
+
+    public void growObstacles(Graphics g) {
+    	// Iterate through each obstacle
+    	for (int a = 0; a < obstacles.size(); a++) {
+    		// Iterate through each vertex
+    		ArrayList<Point> verticies = obstacles.get(a).getVerticies();
+    		Obstacle grownObstacle = new Obstacle();
+    		for (int b = 0; b < verticies.size(); b++) {
+    			Point vertex = verticies.get(b);
+    			int x = dataToMapCoord(vertex.getX());
+        		int y = dataToMapCoord(vertex.getY());
+
+    			grownObstacle.addVertex(new Point(y, x));
+    			grownObstacle.addVertex(new Point(y + ROBOT_SIZE, x));
+    			grownObstacle.addVertex(new Point(y, x + ROBOT_SIZE));
+    			grownObstacle.addVertex(new Point(y + ROBOT_SIZE, x + ROBOT_SIZE));
+    		}
+    		grahamAlg(grownObstacle.getVerticies());
+    	}
+    }
+
+    /**
+     * Graham's algorithm to find the convex hull of a graph. It first finds the lowest point on the graph (looks
+     * at the y value). It then sorts the other points based on its angular relationship with the first point.
+     * Then, it pushes the left points onto a stack and creates the convex hull.
+     */
+    public void grahamAlg(ArrayList<Point> list)
+    {
+    	// finds the bottom & right most point
+        Point first = findFirst(list); 
+        ArrayList<Point> convex = new ArrayList<Point>(list);
+        // sets the data ready for convex hull
+        for (int x = 0; x < convex.size(); x++) {
+            convex.get(x).setFirst(first);
+            convex.get(x).findAngle(first);
+        }
+
+        //sorts the points in angular order
+        insertionSort(convex); 
+
+        Stack<Point> stack = new Stack<Point>();
+        stack.push(convex.get(convex.size()-1));
+        stack.push(first);
+
+        int i = 1;
+        while (i < convex.size()) {
+            Point temp = convex.get(i);
+            Point top = stack.pop();
+            Point secondTop = stack.peek();
+            stack.push(top);
+
+            double sign = (secondTop.getY()-top.getY())*(temp.getX()-secondTop.getX())-(secondTop.getX()-top.getX())*(temp.getY()-secondTop.getY());
+            if (sign <= 0) {
+                stack.push(temp);
+                i++;
+            } else {
+                stack.pop();
+            }
+        }
+
+        ArrayList<Point> temp = new ArrayList<Point>(stack);
+        Obstacle grownObstacle = new Obstacle();
+        for (i = 0; i < temp.size(); i++) {
+        	double x = mapToDataCoord((int)temp.get(i).getX());
+        	double y = mapToDataCoord((int)temp.get(i).getY());
+        	grownObstacle.addVertex(new Point(y, x));
+        }
+        grownObstacles.add(grownObstacle);
+    }
+
+    /**
+     * Finds the first (right and lowermost) point on the graph to start Graham's algorithm.
+     */
+    public Point findFirst(ArrayList<Point> list)
+    {
+        Point temp = list.get(0);
+        int x1 = dataToMapCoord(temp.getX());
+        int y1 = dataToMapCoord(temp.getY());
+        for (int a = 1; a < list.size(); a++) {
+            Point i = list.get(a);
+            int x2 = dataToMapCoord(i.getX());
+        	int y2 = dataToMapCoord(i.getY());
+            if (x2 > x1 || (x2 == x1 && y2 > y1)) {
+                temp = i;
+            }
+        }
+        return temp;
+    }
+
+    public void insertionSort(ArrayList<Point> a)
+    {
+    	int left = 0;
+    	int right = a.size();
+        for( int p = left + 1; p < right; p++ )
+        {
+            Point tmp = a.get(p);
+            int j;
+
+            for( j = p; j > left && tmp.compareTo( a.get(j - 1 ) ) < 0; j-- )
+                a.set(j, a.get( j - 1 ));
+            a.set(j, tmp);
         }
     }
 
@@ -167,7 +275,11 @@ class PointsPanel extends JPanel
      * FillRect only takes int, so need to scale up (by 50) for accuracy 
      * Add 300 to make origin in the center of the panel
      */
-    public int dataToMapCoord(double value){
-    	return (int)(value*50 + 300);
+    public int dataToMapCoord(double value) {
+    	return (int)(value * SCALE_FACTOR + 300);
+    }
+
+    public double mapToDataCoord(int value) {
+    	return ((double)value - 300) / SCALE_FACTOR;
     }
 }
--- Point.java
+++ Point.java
@@ -1,6 +1,9 @@
 public class Point {
 	private double x;
     private double y;
+    private Point first;
+    private double angle;
+
 
     public Point(double x, double y) {
         this.x = x;
@@ -14,7 +17,52 @@ public class Point {
     public double getY() {
         return y;
     }
-    
+
+    public double getAngle() {
+        return angle;
+    }
+
+    public void setFirst(Point frst) {
+        first = frst;
+    }
+
+    public void findAngle(Point point)
+    {
+        angle = (-1)*Math.toDegrees(Math.atan2(y-point.getY(), x-point.getX()));
+        if (angle < 0)
+            angle += 360;
+    }
+
+    public double findDist(Point point)
+    {
+        return Math.sqrt((x-point.getX())*(x-point.getX()) + (y-point.getY())*(y-point.getY()));
+    }
+
+    public int compareTo(Point point)
+    {
+        if (point.getAngle() > angle)
+            return -1;
+        else if (point.getAngle() < angle)
+            return 1;
+        else
+        {
+            double a = Math.sqrt((first.getY()-y)*(first.getY()-y) + (first.getX()-x)*(first.getX()-x));
+            double b = Math.sqrt((first.getY()-point.getY())*(first.getY()-point.getY())+ (first.getX()-point.getX())*(first.getX()-point.getX()));
+            if (a < b)
+                return -1;
+            else
+                return 1;
+        }
+    }
+
+    public boolean equals(Point p)
+    {
+        if (p.getX() == x && p.getY() == y)
+            return true;
+        else
+            return false;
+    }
+
     /*
 	 * Returns a Point representing the vertex in parts.
 	 * parts[0] is assumed to be x and parts[1] is assumed to be y
